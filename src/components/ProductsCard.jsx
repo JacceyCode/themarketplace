555596457da576e1e3ff44259ed4857ae0fee90b
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-11-12 18:08:51 +0100

    product card completed

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
+import { FaArrowRight } from "react-icons/fa6";
+
 function ProductsCard({ product }) {
   return (
-    <section className="group">
+    <section className="group relative">
       <section className="h-96 w-full cursor-pointer overflow-hidden">
         <img
           className="h-full w-full object-cover duration-500 group-hover:scale-110"
@@ -16,15 +18,30 @@ function ProductsCard({ product }) {
             {product.title.substring(0, 16)}
           </h2>
 
-          <section className="relative flex w-24 gap-2 overflow-hidden">
+          <section className="relative flex w-28 justify-end gap-2 overflow-hidden text-sm">
             <section className="flex transform gap-2 transition-transform duration-500 group-hover:translate-x-24">
               <p className="text-gray-500 line-through">${product.oldPrice}</p>
               <p className="font-semibold">${product.price}</p>
             </section>
-            <p className="absolute top-0 z-20 w-[100px] -translate-x-32 transform cursor-pointer text-gray-500 transition-transform duration-500 hover:text-gray-900 group-hover:translate-x-0">
+            <p className="absolute top-0 z-20 flex w-[100px] -translate-x-32 transform cursor-pointer items-center gap-1 text-gray-500 transition-transform duration-500 hover:text-gray-900 group-hover:translate-x-0">
               Add to cart
+              <span>
+                <FaArrowRight />
+              </span>
             </p>
           </section>
+        </section>
+
+        <section>
+          <p>{product.category}</p>
+        </section>
+
+        <section className="absolute right-0 top-4">
+          {product.isNew && (
+            <p className="font-titleFont bg-black px-6 py-1 font-semibold text-white">
+              Sale
+            </p>
+          )}
         </section>
       </section>
     </section>
