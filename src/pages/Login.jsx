f25ee64e029db50d237f2d38ed2721031e8f6fe2
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-11-14 13:02:28 +0100

    google auth enabled

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,62 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
 import { IconContext } from "react-icons";
 import { FaGithub } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
+// import { useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
 
 function Login() {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  //   const dispatch = useDispatch();
+
+  const handleGoogleLogin = (e) => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        toast.success("Log Out Successful!");
+        // dispatch(removeUser());
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+        toast.error("Log Out failed!");
+      });
+  };
+
   return (
     <section className="flex w-full flex-col items-center justify-center gap-10 py-20">
       <section className="flex w-full items-center justify-center gap-10">
-        <section className="flex h-12 w-60 cursor-pointer items-center justify-center gap-2 rounded-md border border-gray-400 text-base tracking-wide duration-300 hover:border-blue-600">
+        <section
+          onClick={handleGoogleLogin}
+          className="flex h-12 w-60 cursor-pointer items-center justify-center gap-2 rounded-md border border-gray-400 text-base tracking-wide duration-300 hover:border-blue-600"
+        >
           <IconContext.Provider value={{ size: "32px" }}>
             <FcGoogle />
           </IconContext.Provider>
           <span className="text-sm text-gray-900">Sign in with Google</span>
         </section>
-        <button className="rounded-md bg-black px-8 py-3 text-base tracking-wide text-white duration-300 hover:bg-gray-800">
+        <button
+          onClick={handleSignOut}
+          className="rounded-md bg-black px-8 py-3 text-base tracking-wide text-white duration-300 hover:bg-gray-800"
+        >
           Sign Out
         </button>
       </section>
@@ -28,6 +72,19 @@ function Login() {
           Sign Out
         </button>
       </section>
+
+      <ToastContainer
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </section>
   );
 }
