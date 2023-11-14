c618def26eee9fd8f00d5183a29cf00f6ed4dc42
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-11-14 11:57:30 +0100

    firebase config added

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNENxuJssCoJe9PdBfeZsAmQXqPmuvLS4",
+  authDomain: "dmarketplaceapp.firebaseapp.com",
+  projectId: "dmarketplaceapp",
+  storageBucket: "dmarketplaceapp.appspot.com",
+  messagingSenderId: "1076684473933",
+  appId: "1:1076684473933:web:e2c0ded95d9d0635d9fe04",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 // import { store } from "./redux/store.js";
 import { store, persistor } from "./redux/store.js";
 import "react-toastify/dist/ReactToastify.css";
+import { app } from "./firebase.config.js";
 import "./index.css";
 import App from "./App.jsx";
 import { PersistGate } from "redux-persist/integration/react";
@@ -18,7 +19,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store} app={app}>
       <PersistGate loading={"loading"} persistor={persistor}>
         <App />
       </PersistGate>
