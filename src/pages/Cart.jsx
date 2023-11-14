28609b600af29010612a105f3139e4d05df22ec8
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-11-14 15:17:31 +0100

    stripe payment link integrated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,14 @@
 import { useSelector } from "react-redux";
-import CartItem from "../components/CartItem";
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import StripeCheckout from "react-stripe-checkout";
+import CartItem from "../components/CartItem";
 
 function Cart() {
   const productData = useSelector((store) => store.market.productData);
+  const userInfo = useSelector((store) => store.market.userInfo);
   const [totalAmt, setTotalAmt] = useState();
+  const [payNow, setPayNow] = useState(false);
 
   useEffect(() => {
     let price = 0;
@@ -14,6 +18,14 @@ function Cart() {
     });
     setTotalAmt(price.toFixed(2));
   }, [productData]);
+
+  const handleCheckOut = () => {
+    if (userInfo) {
+      setPayNow(true);
+    } else {
+      toast.error("Please Sign In to Checkout");
+    }
+  };
 
   return (
     <section>
@@ -45,11 +57,43 @@ function Cart() {
           <p className="font-titleFont mt-6 flex justify-between font-semibold">
             Total <span className="text-xl font-bold">$ {totalAmt}</span>
           </p>
-          <button className="mt-6 w-full bg-black py-3 text-base text-white duration-300 hover:bg-gray-800">
+          <button
+            onClick={handleCheckOut}
+            // disabled={!userInfo}
+            className="mt-6 w-full bg-black py-3 text-base text-white duration-300 hover:bg-gray-800"
+          >
+            {/* {userInfo ? "Proceed to checkout" : "Sign in to checkout"} */}
             Proceed to checkout
           </button>
+
+          {payNow && (
+            <section className="mt-6 flex w-full items-center justify-center">
+              <StripeCheckout
+                stripeKey="pk_test_51OCN8SFrqnEhzr3vm5joLKAOYU6sNV98XaAcxe9YdwzxcxA5wNWR10cvHSgektP0NJo2xvTYaT7thul8ZmzBD0Uj00JLMxNbJ7"
+                name="D Market Place"
+                amount={totalAmt * 100}
+                label="Pay to DMarket"
+                description={`Your payment amount is $${totalAmt}`}
+                // token={payment}
+                email={userInfo.email}
+              />
+            </section>
+          )}
         </section>
       </section>
+
+      <ToastContainer
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </section>
   );
 }
